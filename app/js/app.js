110cd73863ef1c89379e8df1e61dd3c4ddbfc8f8
Ferdi Dirgantara <ferdifordev@gmail.com>
2021-02-09 22:01:16 +0700

    desktop version

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,11 @@
 let slider = document.querySelector(".slider");
 let views = document.querySelector(".views");
 let price = document.querySelector(".price");
+
 views.innerHTML = slider.value;
-price.innerHTML = "$" + (0.16 * slider.value).toPrecision(4);
+price.innerHTML = "$" + (0.16 * slider.value).toFixed(2);
+
 slider.oninput = function () {
   views.innerHTML = this.value;
-  price.innerHTML = "$" + (0.16 * this.value).toPrecision(4);
+  price.innerHTML = "$" + (0.16 * this.value).toFixed(2);
 };
